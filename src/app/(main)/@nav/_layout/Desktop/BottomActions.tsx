1bdfbc8a22fc3bbf8f31ce039d2b73aa11423036
AlwaysOnline <skyoycom@gmail.com>
2024-05-24 09:37:50 +0800

    Update BottomActions.tsx

--- src/app/(main)/@nav/_layout/Desktop/BottomActions.tsx
+++ src/app/(main)/@nav/_layout/Desktop/BottomActions.tsx
@@ -1,23 +1,16 @@
 import { ActionIcon } from '@lobehub/ui';
-import { Book, Github } from 'lucide-react';
+import { Github } from 'lucide-react';
 import Link from 'next/link';
 import { memo } from 'react';
 import { useTranslation } from 'react-i18next';
-
-import { DOCUMENTS, GITHUB } from '@/const/url';
 
 const BottomActions = memo(() => {
   const { t } = useTranslation('common');
 
   return (
-    <>
-      <Link aria-label={'GitHub'} href={GITHUB} target={'_blank'}>
-        <ActionIcon icon={Github} placement={'right'} title={'GitHub'} />
+      <Link aria-label={t('document')} href="https://guilin.app" target={'_blank'}>
+        <ActionIcon icon={Github} placement={'right'} title={t('document')} />
       </Link>
-      <Link aria-label={t('document')} href={DOCUMENTS} target={'_blank'}>
-        <ActionIcon icon={Book} placement={'right'} title={t('document')} />
-      </Link>
-    </>
   );
 });
 
